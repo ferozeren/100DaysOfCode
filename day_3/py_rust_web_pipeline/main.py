82208c06d141559187aaa1844af3ca491e6792ad
ferozeren <ferozeren.60@gmail.com>
2026-05-06 12:33:19 +0530

    initial day_3 progress

---
+++ day_3/py_rust_web_pipeline/main.py
@@ -0,0 +1,10 @@
+from py_rust_web_pipeline import sum_as_string
+
+
+def main() -> None:
+    print(sum_as_string(10, 11))
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ day_3/py_rust_web_pipeline/py_rust_web_pipeline.pyi
@@ -0,0 +1,4 @@
+def sum_as_string(a: int, b: int) -> str:
+    """Takes two integers and returns their sum as a string."""
+    ...
+    return str(a + b)
